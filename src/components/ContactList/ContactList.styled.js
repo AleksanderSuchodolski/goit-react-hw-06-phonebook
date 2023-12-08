2ac024b0119d88ea83e727c3f5f47329ce6bb6ad
AleksanderSuchodolski <129153686+AleksanderSuchodolski@users.noreply.github.com>
2023-12-08 23:26:49 +0100

    copy hw-3 phonebook

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ContactsList = styled.ul`
+  padding: opx;
+  border: 5px solid skyblue;
+  width: 420px;
+  border-radius: 15px;
+  font-size: 20px;
+`;
